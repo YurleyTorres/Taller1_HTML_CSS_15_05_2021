c0c11563a21344614392921649b95d6f02af9a5d
YurleyTorres <marly.torres.silva@gmail.com>
2021-05-27 22:46:28 -0500

    20210527

---
+++ informacion.js
@@ -0,0 +1,25 @@
+let data = [
+    {
+        titulo: "Creaciones Camily",
+        article: {
+            titulo: '',
+            header: "QUIENES SOMOS",
+            div: "Creaciones Camily es una microempresa colombiana que fabrica ropa infantil, tenemos 15 años de experiencia en el mercado, distribuimos en todas las ciudades del país y siempre entregamos prendas de excelente calidad, a buen precio y con diseños hermosos.",
+            aside: "https://www.youtube.com/embed/AF2HXId2Xhg",
+            p: {
+                src: "img/Portada.png",
+                title: "Tenemos una colección llena de color para los peques"
+            }
+        },
+        article2: {
+            titulo: "Elige la talla de ropa IDEAL para tus hijos",
+            header: "Lo primero que debes diferenciar y entender, es que las tallas para bebé y para niños, son distintas.  Las tallas entre 0 y 3 años son para bebés y es medida por meses , las tallas infantiles son de 3 a 7 años.",
+            div: "Tallas para Bebe Niña",
+            p: ' Con este cuadro de medidas puedes elegir tu talla correcta',
+            footer:{
+                src: "img/TallasBebe.jpg",
+                // title: "bbc.com/mundo/noticias"
+            }
+        }
+    }
+];
